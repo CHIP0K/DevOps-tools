b9bf13f9b206b6bec91e31cc4dd0bf75e3f67f76
Ihor Stetsiuk <stetsyukia@gmail.com>
2023-06-25 15:43:35 +0300

    Update script gitleaks-check.sh
    Signed-off-by: Ihor Stetsiuk <stetsyukia@gmail.com>

--- git-hooks/gitleaks-setup.sh
+++ git-hooks/gitleaks-setup.sh
@@ -94,13 +94,62 @@ install_on_unix() {
 }
 
 installPreCommitHOOK() {
-    curl -sSl
+    [[ -d "${INSTALL_APP_PATH}"/git-hook ]] || mkdir -p "${INSTALL_APP_PATH}"/git-hook
+    [[ -x "${INSTALL_APP_PATH}"/git-hook/gitleaks-check.sh ]] || cat > "${INSTALL_APP_PATH}"/git-hook/gitleaks-check.sh <<'EOF'
+#!/usr/bin/env bash
+
+GREEN='\033[0;32m'  # Green
+YELLOW="\033[0;33m" # Yellow
+RED="\033[0;31m"    # Red
+COLOR_OFF="\033[0m" # Text Reset
+
+help() {
+    echo -e "Enable security check for ${0##*/}:\n
+    1) Run this command
+        ${GREEN}git config --global core.hooksPath ${YELLOW}/path/to/script/${COLOR_OFF}
+    "
+}
+
+check() {
+    # Run gitleaks with parameters
+    gitleaks detect --report-format json --redact -v --log-opts "HEAD~1..HEAD"
+
+    # Return code
+    if [[ $? -ne 0 ]]; then
+        if [[ "$LANG" = "uk_UA.UTF-8" ]]; then
+          echo -e "${RED}Помилка: Знайдені проблеми з безпекою в коді.${COLOR_OFF}"
+          exit 1
+        else
+          echo -e "${RED}Error: Security issues have been found in the code.${COLOR_OFF}"
+          exit 1
+        fi
+    else
+        # Success check
+        exit 0
+    fi
+}
+
+case $1 in
+    help)
+    help
+    ;;
+    *)
+    check
+    ;;
+esac
+
+EOF
+
+    chmod +x "${INSTALL_APP_PATH}"/git-hook/gitleaks-check.sh
 }
 
 main() {
     checkDependencies
-    check_arch && check_os
+    check_arch
+    check_os
     install_on_unix
+    installPreCommitHOOK \
+    && "${INSTALL_APP_PATH}"/git-hook/gitleaks-check.sh help
 }
 
 main
