b81c6bb28c5c24a7e3bd2c2b0511b7691c66ec1f
Ihor Stetsiuk <stetsyukia@gmail.com>
2023-07-10 11:30:15 +0300

    Update script: shell-scripts/mysql/mysqldump.sh
    Signed-off-by: Ihor Stetsiuk <stetsyukia@gmail.com>

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |-|-|
 | [iptables_rate_limit.sh](shell-scripts/iptables_rate_limit.sh) | Rate limit requests with iptables |
 |[sentry-backup.sh](shell-scripts/sentry-backup.sh)| Backup and upload sentry dump to nexus storage |
+| [mysqldump.sh](shell-scripts/mysql/mysqldump.sh) | Backup and upload Mysql databases to S3 storage |
--- shell-scripts/mysql/mysqldump.sh
+++ shell-scripts/mysql/mysqldump.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 source /etc/environment
-MYSQL_CONFIG_PATH="${1:-~/.my.cnf}"
 
+MYSQL_CONFIG_PATH="${1:-~/.my.cnf}"
 DUMP_FORMAT="${2:-table}" # Avaliable formats "table|full|all", default table
 DUMP_PATH="${3:-/opt/backups/}"
+
+TODAY=$(date +"%F")
+DUMP_HOST_IDENTITY="${HOSTNAME_}${TODAY}"
+DUMP_TIME=$(date +"%F_%R")
+
 DUMP_ROTATE_DAYS="3"
-DUMP_HOST_IDENTITY="$(hostname)_$(date +"%Y-%m-%d")"
-DUMP_TIMESTAMP="$(date +"%Y-%m-%d_%s")"
+COPY_TO_S3="yes"
+S3_DUMP_ROTATE_DAYS=30
 S3CMD_BUCKET="db.backup/mysqldump-for-slave"
 
 BACKUP_DATABASES="
@@ -41,20 +46,26 @@ dumpPrepare() {
     # Create Backup sub-directories
     MBD="${DUMP_PATH}/${DUMP_HOST_IDENTITY}/mysql"
     install -d "${MBD}"
-
     find "${DUMP_PATH}" -type f -mtime +"${DUMP_ROTATE_DAYS}" -delete # Remove old files
 }
 
 createDumpTable() {
     echo -e "Create a mysql dump tables"
     for db in $BACKUP_DATABASES; do
-        DB_PATH="${MBD}/${db}-table-${DUMP_TIMESTAMP}"
+        DB_PATH="${MBD}/${db}.table.${DUMP_TIME}"
         install -d "${DB_PATH}"
         for dump_table in $(mysql --defaults-file="${MYSQL_CONFIG_PATH}" -Bse "show tables from \`${db}\`"); do
             FILE_TABLE_DATA="${DB_PATH}/${dump_table}.sql"
             FILE_TABLE_SCHEMA="${DB_PATH}/${dump_table}-schema.sql"
-            mysqldump --no-tablespaces --set-gtid-purged=OFF --no-data --single-transaction "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_SCHEMA}.gz"
-            mysqldump --no-tablespaces --set-gtid-purged=OFF --single-transaction "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_DATA}.gz"
+            mysqldump --no-tablespaces \
+                --set-gtid-purged=OFF \
+                --no-data \
+                --single-transaction \
+                "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_SCHEMA}.gz"
+            mysqldump --no-tablespaces \
+                --set-gtid-purged=OFF \
+                --single-transaction \
+                "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_DATA}.gz"
         done
     done
 }
@@ -62,7 +73,7 @@ createDumpTable() {
 createDumpFull() {
     echo -e "Create a mysql dump full"
     for db in $BACKUP_DATABASES; do
-        DB_PATH="${MBD}/${db}-full-${DUMP_TIMESTAMP}"
+        DB_PATH="${MBD}/${db}.full.${DUMP_TIME}"
         install -d "${DB_PATH}"
         mysqldump --single-transaction \
             --quick --add-drop-database \
@@ -93,20 +104,37 @@ createDump() {
     esac
 }
 
+s3RotateDumps() {
+    OLDER_THOSE_DAYS=$(date --date "${S3_DUMP_ROTATE_DAYS} days ago" +%s)
+    for db_dump in ${BACKUP_DATABASES}; do
+        s3cmd ls s3://"${S3CMD_BUCKET}"/"${db_dump}"."${DUMP_FORMAT}" | awk '{print $2}'
+    done |
+        while read -r line; do
+            CREATED_DAY=$(echo -e "${line}" | awk -F. 'END {print $(NF)}' | tr '/_' ' ')
+            CREATED_DAY_TIMESTAMP=$(date -d "${CREATED_DAY}" +%s)
+            if [[ ${CREATED_DAY_TIMESTAMP} -lt ${OLDER_THOSE_DAYS} ]]; then
+                echo -e "delete: ${line}"
+                s3cmd del --recursive "${line}"
+            fi
+        done
+}
+
 pushDumpS3CMD() {
-    COPY_DUMPS=$(find "${MBD}" -type d -name *"${DUMP_TIMESTAMP}" -exec basename \{} \;)
+    COPY_DUMPS=$(find "${MBD}" -type d -name *"${DUMP_TIME}" -exec basename \{} \;)
     for DUMP_NAME in $COPY_DUMPS; do
         s3cmd sync "${MBD}"/"${DUMP_NAME}"/* s3://"${S3CMD_BUCKET}"/"${DUMP_NAME}"/ &>/dev/null
     done
 }
 
-
 main() {
     checkDependencies &&
         checkLock &&
         dumpPrepare &&
-        createDump &&
-        pushDumpS3CMD
+        createDump
+    if [[ ${COPY_TO_S3} == "true" ]] || [[ ${COPY_TO_S3} == "yes" ]]; then
+        pushDumpS3CMD &&
+            s3RotateDumps
+    fi
 }
 
 main
