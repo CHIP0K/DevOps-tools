30e7e1e077a103d2bae13fea247a09bac6c0614d
Ihor Stetsiuk <ihor.stetsiuk@icloud.com>
2025-01-15 11:50:44 +0200

    Update mysqldump.sh script
    Signed-off-by: Ihor Stetsiuk <ihor.stetsiuk@icloud.com>

--- shell-scripts/mysql/mysqldump.sh
+++ shell-scripts/mysql/mysqldump.sh
@@ -1,33 +1,74 @@
 #!/usr/bin/env bash
+#-------------------------------------------------------------------------------
+# DESCRIPTION:
+#   This script generates MySQL database backups using mysqldump. It is designed
+#   to run in a controlled environment where the SHELL is set to /bin/bash,
+#   especially when used in a crontab entry.
+#
+# USAGE:
+#   ./mysqldump.sh /path/to/.my.cnf table /path/to/backups
+#   - /path/to/.my.cnf  : Path to MySQL credentials file (contains user & password).
+#   - table             : Name of the table to dump (optional if dumping all).
+#   - /path/to/backups  : Directory to store backups.
+#
+# ENVIRONMENT:
+#   - SHELL=/bin/bash: Ensures that environment variables and shell behavior
+#     match interactive Bash usage.
+#   - CRON: When running from crontab, the environment may differ from an
+#     interactive session, so explicitly setting SHELL to /bin/bash is recommended.
+#
+# CRON EXAMPLE:
+#   SHELL=/bin/bash
+#   0 8 * * * /path/to/mysqldump.sh /path/to/.my.cnf table /path/to/backups
+#-------------------------------------------------------------------------------
+
 source /etc/environment
 
 MYSQL_CONFIG_PATH="${1:-~/.my.cnf}"
 DUMP_FORMAT="${2:-table}" # Avaliable formats "table|full|all", default table
-DUMP_PATH="${3:-/opt/backups/}"
+DUMP_PATH="${3:-/opt/backups}"
 
 TODAY=$(date +"%F")
-DUMP_HOST_IDENTITY="${HOSTNAME_}${TODAY}"
+DUMP_HOST_IDENTITY="${HOSTNAME}-${TODAY}"
 DUMP_TIME=$(date +"%F_%R")
 
-DUMP_ROTATE_DAYS="3"
+DUMP_ROTATE_DAYS="0"
 COPY_TO_S3="yes"
 S3_DUMP_ROTATE_DAYS=30
-S3CMD_BUCKET="db.backup/mysqldump-for-slave"
+S3CMD_BUCKET="db.backup/databases"
 
 BACKUP_DATABASES="
-db1
-db2
+database1
+database2
+"
+IGNORE_TABLES_DATA="
+table1
+table2
 "
 
-checkLock() {
+readonly \
+    MYSQL_CONFIG_PATH \
+    DUMP_FORMAT \
+    DUMP_PATH \
+    TODAY \
+    DUMP_HOST_IDENTITY \
+    DUMP_TIME \
+    DUMP_ROTATE_DAYS \
+    COPY_TO_S3 \
+    S3_DUMP_ROTATE_DAYS \
+    S3CMD_BUCKET \
+    BACKUP_DATABASES \
+    IGNORE_TABLES_DATA
+
+check_lock_running_script() {
     if [[ $(pgrep -fc "${0##*/}") -gt 1 ]]; then
         echo "Script ${0##*/} is running"
         exit 1
     fi
 }
 
-checkDependencies() {
-    deps=(mysql mysqldump find gzip s3cmd)
+check_dependencies() {
+    deps=(mysqldump find gzip s3cmd)
     function installed {
         cmd=$(command -v "${1}")
         [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
@@ -42,14 +83,13 @@ checkDependencies() {
     done
 }
 
-dumpPrepare() {
+dump_prepare() {
     # Create Backup sub-directories
     MBD="${DUMP_PATH}/${DUMP_HOST_IDENTITY}/mysql"
     install -d "${MBD}"
-    find "${DUMP_PATH}" -type f -mtime +"${DUMP_ROTATE_DAYS}" -delete # Remove old files
 }
 
-createDumpTable() {
+create_dump_table() {
     echo -e "Create a mysql dump tables"
     for db in $BACKUP_DATABASES; do
         DB_PATH="${MBD}/${db}.table.${DUMP_TIME}"
@@ -57,20 +97,24 @@ createDumpTable() {
         for dump_table in $(mysql --defaults-file="${MYSQL_CONFIG_PATH}" -Bse "show tables from \`${db}\`"); do
             FILE_TABLE_DATA="${DB_PATH}/${dump_table}.sql"
             FILE_TABLE_SCHEMA="${DB_PATH}/${dump_table}-schema.sql"
-            mysqldump --no-tablespaces \
+            mysqldump  --defaults-file="${MYSQL_CONFIG_PATH}" \
+                 --no-tablespaces \
                 --set-gtid-purged=OFF \
                 --no-data \
                 --single-transaction \
                 "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_SCHEMA}.gz"
-            mysqldump --no-tablespaces \
-                --set-gtid-purged=OFF \
-                --single-transaction \
-                "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_DATA}.gz"
+            if [[ -z ${IGNORE_TABLES_DATA} ]] || [[ ${IGNORE_TABLES_DATA} != *"${dump_table}"* ]]; then
+                mysqldump  --defaults-file="${MYSQL_CONFIG_PATH}" \
+                    --no-tablespaces \
+                    --set-gtid-purged=OFF \
+                    --single-transaction \
+                    "${db}" "${dump_table}" | gzip -9 >"${FILE_TABLE_DATA}.gz"
+            fi
         done
     done
 }
 
-createDumpFull() {
+create_full_dump() {
     echo -e "Create a mysql dump full"
     for db in $BACKUP_DATABASES; do
         DB_PATH="${MBD}/${db}.full.${DUMP_TIME}"
@@ -86,17 +130,17 @@ createDumpFull() {
     done
 }
 
-createDump() {
+create_dump() {
     case $DUMP_FORMAT in
     table)
-        createDumpTable
+        create_dump_table
         ;;
     full)
-        createDumpFull
+        create_full_dump
         ;;
     all)
-        createDumpTable
-        createDumpFull
+        create_dump_table
+        create_full_dump
         ;;
     *)
         echo "Avaliable formats: \"table|full|all\", default: table"
@@ -104,36 +148,52 @@ createDump() {
     esac
 }
 
+rotate_local_dumps() {
+    for database in ${BACKUP_DATABASES}; do
+        local older_those_days=$(date --date "${DUMP_ROTATE_DAYS} days ago" +%s)
+        find "${DUMP_PATH:-/opt/backups}" -type d -name *${database}* |
+            while read -r line; do
+                local created_day=$(echo -e "${line}" | awk -F. 'END {print $(NF)}' | tr '/_' ' ')
+                local created_day_timestamp=$(date -d "${created_day}" +%s)
+                if [[ ${created_day_timestamp} -lt ${older_those_days} ]]; then
+                    rm -rf "${line:-/opt/backups}"
+                fi
+            done
+    done
+}
+
 s3RotateDumps() {
-    OLDER_THOSE_DAYS=$(date --date "${S3_DUMP_ROTATE_DAYS} days ago" +%s)
+    local older_those_days=$(date --date "${S3_DUMP_ROTATE_DAYS} days ago" +%s)
     for db_dump in ${BACKUP_DATABASES}; do
         s3cmd ls s3://"${S3CMD_BUCKET}"/"${db_dump}". | awk '{print $2}'
     done |
         while read -r line; do
-            CREATED_DAY=$(echo -e "${line}" | awk -F. 'END {print $(NF)}' | tr '/_' ' ')
-            CREATED_DAY_TIMESTAMP=$(date -d "${CREATED_DAY}" +%s)
-            if [[ ${CREATED_DAY_TIMESTAMP} -lt ${OLDER_THOSE_DAYS} ]]; then
+            local created_day=$(echo -e "${line}" | awk -F. 'END {print $(NF)}' | tr '/_' ' ')
+            local created_day_timestamp=$(date -d "${created_day}" +%s)
+            if [[ ${created_day_timestamp} -lt ${older_those_days} ]]; then
                 s3cmd del --recursive "${line}"
             fi
         done
 }
 
 pushDumpS3CMD() {
-    COPY_DUMPS=$(find "${MBD}" -type d -name *"${DUMP_TIME}" -exec basename \{} \;)
-    for DUMP_NAME in $COPY_DUMPS; do
+    local copy_dumps
+    copy_dumps=$(find "${MBD}" -type d -name *"${DUMP_TIME}" -exec basename \{} \;)
+    for DUMP_NAME in $copy_dumps; do
         s3cmd sync "${MBD}"/"${DUMP_NAME}"/* s3://"${S3CMD_BUCKET}"/"${DUMP_NAME}"/ &>/dev/null
     done
 }
 
 main() {
-    checkDependencies &&
-        checkLock &&
-        dumpPrepare &&
-        createDump
+    check_dependencies &&
+        check_lock_running_script &&
+        dump_prepare &&
+        create_dump
     if [[ ${COPY_TO_S3} == "true" ]] || [[ ${COPY_TO_S3} == "yes" ]]; then
         pushDumpS3CMD &&
             s3RotateDumps
     fi
+    rotate_local_dumps
 }
 
 main
